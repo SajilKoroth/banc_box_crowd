44d16bcc1b1ffec2817a07045583cced51b03253
Roger Smith <rogsmith@gmail.com>
2014-06-18 12:09:53 -0400

    api fixes

--- lib/banc_box_crowd/api.rb
+++ lib/banc_box_crowd/api.rb
@@ -52,34 +52,34 @@ module BancBoxCrowd
 		private
 
 		def formatted_time(time)
-	      time && time.strftime('%Y-%m-%d %H:%M:%S')
-	    end
+      time && time.strftime('%Y-%m-%d %H:%M:%S')
+    end
 
-	    def formatted_date(date)
-	      date && date.strftime('%Y-%m-%d')
-	    end
+    def formatted_date(date)
+      date && date.strftime('%Y-%m-%d')
+    end
 
-	    def boolean_to_y_n(bool)
-	      bool ? 'Y' : 'N'
-	    end
+    def boolean_to_y_n(bool)
+      bool ? 'Y' : 'N'
+    end
 
-	    def boolean_to_yes_no(bool)
-	      bool ? 'Yes' : 'No'
-	    end
+    def boolean_to_yes_no(bool)
+      bool ? 'Yes' : 'No'
+    end
 
 		def get_response(method, endpoint, data)
 			ignore_exceptions = data.has_key?(:ignore_exceptions) && data.delete(:ignore_exceptions)	
-	      	response = connection.__send__(method, endpoint, data)
-	      	unless ignore_exceptions
-		    	if response['error'] != nil
-		        	raise BancBoxCrowd::Error.new(response['error'])
-		      	end
-		  	end
-	      	response
-    	end
+	    response = connection.__send__(method, endpoint, data)
+	    unless ignore_exceptions
+		    if response['error'] != nil
+	      	raise BancBoxCrowd::Error.new(response['error'])
+	      end
+		  end
+	    response
+   	end
 
-    	def object_from_response(klass, method, endpoint, data)
-      		klass.from_response(get_response(method, endpoint, data))
-    	end
+  	def object_from_response(klass, method, endpoint, data)
+    		klass.from_response(get_response(method, endpoint, data))
+  	end
 	end
 end
